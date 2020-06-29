a02b7b54ca7e1d76cb07e4fcada7962c1e83e90f
Mauro Scomparin <scompo@gmail.com>
2020-06-29 22:18:18 +0200

    Added router and log.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
 const Koa = require('koa')
+const Router = require('@koa/router')
+const logger = require('koa-pino-logger')
 const path = require('path')
 const Pug = require('koa-pug')
 
 const app = new Koa()
+const router = new Router()
+
+router.get('/', async ctx => {
+	await ctx.render('home')
+})
 
 const pug = new Pug({
-  viewPath: path.resolve(__dirname, '../views'),
-  app: app
+	viewPath: path.resolve(__dirname, '../views'),
+	app: app
 })
 
 pug.locals.name = 'project-organizer'
 pug.locals.version = require('../package.json').version
 
-app.use(async ctx => {
-  await ctx.render('home')
-})
+app  .use(logger())
+	.use(router.routes())
+	.use(router.allowedMethods())
 
 module.exports = async function (conf) {
-  return app
+	return app
 }
